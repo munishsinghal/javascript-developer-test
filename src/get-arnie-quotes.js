208cacaa1f2b2db84e46cdc219940154e44a97f9
Rachita Gupta <rachitag@gmail.com>
2021-09-11 14:51:32 +1000

    updated function getArnieQuotes in file get-arnie-quotes.js to return httpGet result for input urls

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -3,8 +3,20 @@ const { httpGet } = require('./mock-http-interface');
 const getArnieQuotes = async (urls) => {
   // TODO: Implement this function.
   // return results;
-};
 
+  //additionally below things can be done for improvement
+  // 1. validation
+  // 2. enums for json object key
+  // 3. enums of http codes
+  // 4. separate function can be created for message retrival from json 
+  return Promise.all((urls.map(function (url) {
+    return httpGet(url).then(response => {
+      return (response.status == 200) ?
+        { 'Arnie Quote': JSON.parse(response.body).message }
+        : { 'FAILURE': JSON.parse(response.body).message }
+    });
+  })));
+}
 module.exports = {
   getArnieQuotes,
 };
